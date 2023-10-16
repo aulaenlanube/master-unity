8be3665e17f1fc8def0d8256061e374ec37f042f
aulaenlanube <edtorlla@gmail.com>
2023-10-16 14:15:45 +0200

    video 1-12

--- master-unity/Assets/Tema1/Scripts/Armas.cs
+++ master-unity/Assets/Tema1/Scripts/Armas.cs
@@ -1,6 +1,22 @@
 using System.Text;
 using UnityEngine;
+using UnityEngine.UIElements;
 
+public class Posicion 
+{
+    public int x, y, z;
+    public Posicion(int x, int y, int z) 
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public override string ToString()
+    {
+        return $"{x}:{y}:{z}";
+    }
+}
 public enum TipoArma
 {
     Espada,
@@ -28,16 +44,60 @@ public struct DatosArma
 }
 
 public class Armas : MonoBehaviour
-{
-    DatosArma arma1 = new DatosArma("Cimitarra de la ira", 28, TipoArma.Espada);
-    DatosArma arma2 = new DatosArma("Arco de la guardia", 50, TipoArma.Arco);
-    DatosArma arma3 = new DatosArma("Tridente de escamas", 22, TipoArma.Lanza);
-
+{   
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(arma1);
-        Debug.Log(arma2);
-        Debug.Log(arma3);
+        Vector3 miPosicion1 = Vector3.zero;
+        Posicion miPosicion2 = new Posicion(0, 0, 0);
+        ModificarObjeto(miPosicion1);
+        ModificarObjeto(miPosicion2);
+
+        Debug.Log($"Pos en Start: {miPosicion1}");
+        Debug.Log($"Pos en Start: {miPosicion2}");
+
+        TipoArma miArma = TipoArma.Espada;
+        Debug.Log("Arma antes: " + miArma);         // Arma antes: Espada
+
+        CambiarTipoArma(miArma);                    // Arma actual: Arco
+        Debug.Log("Arma después: " + miArma);	    // Arma después: Espada
+
+        DatosArma arma1 = new DatosArma("Cimitarra de la ira", 28, TipoArma.Espada);
+
+        CambiarDatosArma(arma1);                    //Cimitarra de la ira X 2:56
+        Debug.Log($"{arma1.nombre}:{arma1.daño}"); 	//Cimitarra de la ira:28
     }
+
+    public void CambiarTipoArma(TipoArma armaActual)
+    {
+        armaActual = armaActual switch
+        {
+            TipoArma.Espada => TipoArma.Arco,
+            TipoArma.Arco   => TipoArma.Lanza,
+            TipoArma.Lanza  => TipoArma.Espada,
+            _               => TipoArma.Espada
+        };
+        Debug.Log("Arma actual: " + armaActual);
+    }
+
+    public void CambiarDatosArma(DatosArma datos)
+    {
+        datos.daño *= 2;
+        datos.nombre += " X 2";
+        Debug.Log($"{datos.nombre}:{datos.daño}");
+    }
+
+    public void ModificarObjeto(Vector3 pos)
+    {
+        pos.x += 10;
+        Debug.Log($"Posición en el método ModificarObjeto:{pos}");
+    }
+    public void ModificarObjeto(Posicion pos)
+    {
+        pos.x += 10;
+        Debug.Log($"Posición en el método ModificarObjeto:{pos}");
+    }
+
+
+
 }
